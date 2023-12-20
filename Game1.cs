71c5a817bc9430cdf414c5dcf84e23e3f9e6ecdf
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-12-20 11:04:35 -0500

    added pacman textures

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,8 @@ namespace Monogame_Lesson_4___Keyboard_and_Mouse_Events
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D pacTexture;
+        Rectangle pacLocation;
+        KeyboardState keyboardState;
 
         public Game1()
         {
@@ -22,12 +24,13 @@ namespace Monogame_Lesson_4___Keyboard_and_Mouse_Events
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            pacLocation = new Rectangle(10, 10, 75, 75);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            pacTexture = Content.Load<Texture2D>("PacRight");
+            pacTexture = Content.Load<Texture2D>("PacSleep");
             // TODO: use this.Content to load your game content here
         }
 
@@ -39,6 +42,28 @@ namespace Monogame_Lesson_4___Keyboard_and_Mouse_Events
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+            keyboardState = Keyboard.GetState();
+            pacTexture = Content.Load<Texture2D>("PacSleep");
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacLocation.Y -= 2;
+                pacTexture = Content.Load<Texture2D>("PacUp");
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacLocation.Y += 2;
+                pacTexture = Content.Load<Texture2D>("PacDown");
+            }
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacLocation.X -= 2;
+                pacTexture = Content.Load<Texture2D>("PacLeft");
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacLocation.X += 2;
+                pacTexture = Content.Load<Texture2D>("PacRight");
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -48,6 +73,9 @@ namespace Monogame_Lesson_4___Keyboard_and_Mouse_Events
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(pacTexture, pacLocation, Color.White);
+            _spriteBatch.End();
         }
     }
 }
